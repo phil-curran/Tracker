493e231ab2c9f2ed6005225fe2bc714c23b2501c
Phil Curran <pecurran@hotmail.com>
2022-03-09 14:21:07 -0800

    added passing test for adding new vendor

--- Tracker.Tests/ModelTests/VendorTests.cs
+++ Tracker.Tests/ModelTests/VendorTests.cs
@@ -1,12 +1,56 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tracker.Models;
 
 namespace Tracker.Tests
 {
-  // [TestClass]
-  // public class VendorTests : IDisposable
-  // { 
-  // }
+  [TestClass]
+  public class VendorTests
+  {
 
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    // test AddNewVendor()
+    [TestMethod]
+    public void AddNewVendor()
+    {
+      // Arrange
+      Vendor newVendor = new Vendor("test name", "test description");
+
+      // Act
+
+      // Assert
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+
+    }
+
+    // test List<Order> GetAll()
+    [TestMethod]
+    public void Test1()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+
+    // test List<Order> GetAll()
+    [TestMethod]
+    public void Test2()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+
+  }
 }
